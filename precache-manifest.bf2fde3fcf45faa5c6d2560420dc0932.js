f4f807aa9bc636b6372366f3b3b281d8dc436245
samuelselasi <samelsekasi@gmail.com>
2021-06-01 09:33:30 +0000

    Updates

---
+++ precache-manifest.bf2fde3fcf45faa5c6d2560420dc0932.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "886192da2c20fec876f5e30b668ea8de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d41ab8d0625b7a7b989",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "5deb9d10f2209e9c77e7",
+    "url": "/static/js/2.85fb9bbf.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.85fb9bbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d41ab8d0625b7a7b989",
+    "url": "/static/js/main.3362fe56.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
